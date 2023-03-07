21ff17c6644c3cfd26fb63b7557bb11ebdeafd6e
tungbhrelipa <tungbh@relipasoft.com>
2023-03-07 08:41:28 +0700

    update class<DB> insert data

--- DB.py
+++ DB.py
@@ -1,10 +1,30 @@
 import pandas as pd
+from datetime import datetime
 
 DB_NAME = 'DB.csv'
+DB_LOG = 'DB_Log'
 
 class DB():
-  def insert():
-    print('insert')
+  def insert(data):
+    record = data
+    with open('DB_Log') as f:
+      lines = f.readlines()
+      # read file log to get lastest record id
+      new_id = str(int(lines[2].replace('LAST_ID=', '')) + 1)
+      record['id'] = new_id
+      # append to df
+      df = pd.read_csv(DB_NAME)
+      df_dictionary = pd.DataFrame([record])
+      new_df = pd.concat([df, df_dictionary], ignore_index=True)
+      # save csv
+      new_df.to_csv('DB_1.csv', index=False)
+      # update file log
+      wf = open(DB_LOG,"w")
+      wf.write(f'LAST_UPDATE={datetime.now().isoformat()}\nTOTAL_RECORD={new_id}\nLAST_ID={new_id}')
+      wf.close()
+      f.close()
+
+    return record
 
   def delete():
     print('delete')
