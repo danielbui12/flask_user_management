17d0958fc93270dedf965654052f13c473d05b3b
Nguyễn Chí Quân <93815321+Quan2611@users.noreply.github.com>
2023-03-17 20:35:48 +0700

    update

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-# import pandas as pd
+import pandas as pd
 
-# df = pd.read_csv("DB.csv")
+df = pd.read_csv("DB.csv")
+df = df.fillna(method = 'ffill')
+print(df[df["price"]>0] )
+df.to_csv("DB.csv", index=False)
 # # print(df.iloc[-1]['id'])
 # print(df.iloc[2:301])
 
